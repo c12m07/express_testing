2b79b2b7fc4c1549996a85ea7a660635a66c786c
c12m07 <ximic4@gmail.com>
2022-11-22 18:49:26 -0500

    Pull 2

--- config/config.js
+++ config/config.js
@@ -11,21 +11,21 @@ runDotEnv();
 const config = {
   "development": {
     "username": process.env.DB_USER || "postgres",
-    "password": process.env.DB_PASSWORD,
+    "password": process.env.DB_PASSWORD || "7539182465",
     "database": process.env.DB_NAME || "prueba1",
     "host": process.env.DB_HOST,
     "dialect": "postgres"
   },
   "test": {
     "username": process.env.DB_USER || "postgres",
-    "password": process.env.DB_PASSWORD,
+    "password": process.env.DB_PASSWORD || "7539182465",
     "database": process.env.DB_NAME || "prueba1",
     "host": process.env.DB_HOST,
     "dialect": "postgres"
   },
   "production": {
     "username": process.env.DB_USER || "postgres",
-    "password": process.env.DB_PASSWORD,
+    "password": process.env.DB_PASSWORD || "7539182465",
     "database": process.env.DB_NAME || "prueba1",
     "host": process.env.DB_HOST || "localhost",
     "dialect": "postgres"
