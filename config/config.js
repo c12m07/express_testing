0412d2b75c41680ac58fdacdc0294bb23cf912eb
c12m07 <ximic4@gmail.com>
2022-11-22 22:48:05 -0500

    pruebas

--- config/config.js
+++ config/config.js
@@ -1,35 +1,32 @@
-function runDotEnv() {
-  if (process.env.ENV === 'development') {
-    import('dotenv').then(dotenv => dotenv.config({ path: '.env' }));
-  }
-}
+import dotenv from 'dotenv'
 
-runDotEnv();
+dotenv => dotenv.config({ path: '.env' });
 
 const config = {
-  'development': {
-    'username': process.env.DB_USER || 'postgres',
-    'password': process.env.DB_PASSWORD || '7539182465',
-    'database': process.env.DB_NAME || 'prueba1',
-    'host': process.env.DB_HOST,
-    'logging': true,
-    'dialect': 'postgres'
+  development: {
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || '7539182465',
+    database: process.env.DB_NAME || 'prueba1',
+    host: process.env.DB_HOST,
+    logging: true,
+    dialect: 'postgres'
   },
-  'test': {
-    'username': process.env.DB_USER || 'postgres',
-    'password': process.env.DB_PASSWORD || '7539182465',
-    'database': process.env.DB_NAME || 'prueba1',
-    'host': process.env.DB_HOST,
-    'logging': false,
-    'dialect': 'postgres'
+  test: {
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || '7539182465',
+    database: process.env.DB_NAME || 'prueba1',
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: process.env.DB_PORT || 5432,
+    logging: false,
+    dialect: 'postgres'
   },
-  'production': {
-    'username': process.env.DB_USER || 'postgres',
-    'password': process.env.DB_PASSWORD || '7539182465',
-    'database': process.env.DB_NAME || 'prueba1',
-    'host': process.env.DB_HOST,
-    'logging': true,
-    'dialect': 'postgres'
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    logging: false,
+    dialect: 'postgres'
   }
 }
 
