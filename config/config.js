48424853b9e0899f52901368aa12e69bc4a6f6f7
c12m07 <ximic4@gmail.com>
2022-11-24 23:47:02 -0500

    Add index

--- config/config.js
+++ config/config.js
@@ -18,7 +18,7 @@ const config = {
   },
   test: {
     username: process.env.DB_USER || "postgres",
-    password: process.env.DB_PASSWORD || "postgres",
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME || "prueba1",
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
